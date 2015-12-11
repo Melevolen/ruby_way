72717e56c0874f5272681301217657da5dc24c61
Ilia Diakov <longerd@MacBook-Air1960.local>
2015-12-11 14:47:02 +0300

    Правка типа данных

--- lesson_1/2_triangles_area.rb
+++ lesson_1/2_triangles_area.rb
@@ -3,8 +3,8 @@
 # h - height
 # 1/2*a*h
 puts "Input base: "
-a = gets.chomp.to_i
+a = gets.chomp.to_f
 puts "Input height: "
-h = gets.chomp.to_i
+h = gets.chomp.to_f
 area = 1.0/2 * a * h 
 puts "Area is #{area}"
