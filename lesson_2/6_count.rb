f503746f25126a356979596d7533b2df1815e210
Ilia Diakov <longerd@MacBook-Air1960.local>
2015-12-17 22:20:50 +0300

    number 6

--- lesson_2/6_count.rb
+++ lesson_2/6_count.rb
@@ -15,8 +15,9 @@ end
 
 #all.each {|k, v| puts "You want #{k} and it will be #{v['price']*v['count']}"}
 all.each do |k, v| 
-	puts "You want #{k} and it will be #{v['price']*v['count']}"
-	all_money = all_money + v['price']*v['count']
+	cnt = v['price']*v['count']
+	puts "You want #{k} and it will be #{cnt}"
+	all_money = all_money + cnt
 end
 
 # all.each_value do |v|
