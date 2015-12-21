5c112dfd9949886714495e5c81fff99033260722
Ilia Diakov <longerd@MacBook-Air1960.local>
2015-12-22 00:37:53 +0300

    i love trains

--- lesson_3/1_Railway.rb
+++ lesson_3/1_Railway.rb
@@ -1,22 +1,40 @@
 class RailwayStation
 	attr_accessor :trains 
-
+	attr_reader :name
 	def initialize(name)
 		@name = name
 		@trains = []
+		@trains_cargo = 0
+		@trains_passenger = 0
 	end
-	def train_add (obj) 
-		@trains << obj
+	def train_add (train_obj) 
+		@trains << train_obj
 	end
-	def trains_go_route(obj)
-		@trains.delete(obj)
+	def trains_go_route(train_obj)
+		@trains.delete(train_obj)
 	end 
 	def trains_list
 		puts @trains
 	end
+	def trains_cargo_count
+		@trains.each  do |x|
+			if x.kind == "cargo"
+				@trains_cargo += 1
+			end
+			p @trains_cargo
+		end
+	end
+	def trains_passenger_count
+		@trains.each  do |x|
+			if x.kind == "passenger"
+				@trains_passenger += 1
+			end
+			p @trains_passenger
+		end
+	end
 end
 
-class Route
+class Route #Part done
 	attr_accessor :stations
 
 	def initialize (from, to)
@@ -35,8 +53,8 @@ class Route
 	end
 end
 
-
-class Train
+class Train 
+# Перемещаться между станциями, указанными в маршруте.
 
 	attr_accessor :speed
 	attr_reader :carriages, :kind, :route
@@ -52,23 +70,37 @@ class Train
 	def stop
 		@speed = 0
 	end
-	def carriages
+	def carriages_add
 		if speed == 0
 			@carriages += 1
 		else
 			puts "pls stop the Train! Use 'stop' method for it."
 		end
 	end
-	def route_add(route_name)
-		@route = route_name.stations
+	def route_add(route_name, train_obj)
+		@route = route_name
+		@stn = 0 # Подразумеваю номер станции, когда назначаем маршрут, поезд же с первой станции начинает.
+		@route.stations[@stn].train_add(train_obj)
 	end
 	def station_now 
-		@route[0].name  
+	#	@route.stations.each {|x| p x.name}
+		p	@route.stations[@stn].name
 	end
 	def station_next
+		p	@route.stations[@stn+1].name
 	end
 	def station_previous 
+		if @stn == 0 
+			puts "You at the first station in your Route"
+		else 
+			p	@route.stations[@stn-1].name
+		end
 	end
+	def go_forward(train_obj)
+		@route.stations[@stn].trains_go_route(train_obj)
+		@stn += 1 # && (puts "Go to the next station")
+		@route.stations[@stn].train_add(train_obj)
+	end	
 end
 
 station1 = RailwayStation.new("monino")
@@ -81,11 +113,23 @@ p station3
 route1 = Route.new(station1, station3)
 route1.station_add(station2)
 p route1.stations
+
 train1 = Train.new("cargo", 15)
-train1.route_add(route1)
+train1.route_add(route1, train1)
+
 p train1.route 
-p train1.station_now
+train1.station_next
 
+train1.go_forward(train1) # Едем на следующую станцию
+train1.station_next
+train1.station_previous
 
+train1.go_forward(train1) # Едем на следующую, последнюю в этом маршруте станцию.
+train1.station_now
 
+p train1.carriages #ДОбавляем вагончик
+train1.carriages_add
+p train1.carriages
 
+station3.trains_cargo_count # Считаем кол-во поездов на станции каждого вида.
+station3.trains_passenger_count
