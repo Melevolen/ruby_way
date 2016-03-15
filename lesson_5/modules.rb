973d3c5922082c0490276a943568e77f975b02d9
Ilia Diakov <longerd@MacBook-Air1960.local>
2016-03-15 22:46:55 +0300

    15.03 correcting

--- lesson_5/modules.rb
+++ lesson_5/modules.rb
@@ -4,19 +4,20 @@ end
 
 module InstanceCounter
 	def self.included (base)
-		base.extend ClassMethods
+		base.extend ClassMethods 
 		base.send :include, InstanceMethods
 	end
 	module ClassMethods
 		attr_accessor :instances
-		@instances = 0
+#		@instances = 0
 	end
 
-	module InstanceMethods
+	module InstanceMethods # Метод инстанса
 
 		protected
-		def register_instance
-		  @instances += 1
+		def register_instances
+		  self.class.instances ||= 0 # Иициализирует переменную при первом экземпляре класса, те когда она еще не создана.
+		  self.class.instances += 1 # Увеличивает значение переменной, обращаемся к переменной КЛАССА  "self.class.variable"
 		end
 	end
 end
